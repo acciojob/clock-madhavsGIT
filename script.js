fdaaafef55a79b0ce798cd82628257b0278743be
madhav <163291389+madhavsGIT@users.noreply.github.com>
2024-08-02 09:42:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function updateTimer() {
+	const timerElement = document.getElementById("timer");
+	const now = new Date();
+	const year = now.getFullYear();
+	const month = now.getMonth() + 1;	
+}
